e2492b987edfa8e8c3f4e8ebaa1bd07d831d3e47
vldmrgrbv <vovagribov@mail.ru>
2019-05-19 14:55:47 +0700

    Add project SportsStore

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent 
+    {
+        private IProductRepository repository;
+
+        public NavigationMenuViewComponent(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+
+        }
+    }
+}
---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize = 4;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List(string category, int page = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .Where(p => category == null || p.Category == category)
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                },
+                CurrentCategory = category
+            });
+    }
+}
